d4f8624a5640ef7fa92814d937b0c12927d12982
vabouque <valentin-bouquet@hotmail.fr>
2016-10-10 17:18:22 +0200

    Add class ProductC

---
+++ src/main/java/fr/paris10/poa/td3/main/ProductC.java
@@ -0,0 +1,11 @@
+package fr.paris10.poa.td3.main;
+
+/**
+ * Created by vabouque on 10/10/2016.
+ */
+public class ProductC {
+    public void perform() {
+        System.out.println("I'm a ProductC, " +
+                "performing");
+    }
+}
