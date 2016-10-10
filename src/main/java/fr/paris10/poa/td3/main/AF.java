d765a93c29cd18e2eaf2f316e3670b863030c645
vabouque <valentin-bouquet@hotmail.fr>
2016-10-10 17:48:51 +0200

    Add class AF

---
+++ src/main/java/fr/paris10/poa/td3/main/AF.java
@@ -0,0 +1,13 @@
+package fr.paris10.poa.td3.main;
+
+import fr.paris10.poa.td3.client.Client;
+
+/**
+ * Created by vabouque on 10/10/2016.
+ */
+public class AF {
+    public static void main(String args) {
+        Client myClient = new Client();
+        myClient.foo();
+    }
+}
