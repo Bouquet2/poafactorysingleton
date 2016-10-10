f713460a154ad2637a8d5355b902760d0968e23a
vabouque <valentin-bouquet@hotmail.fr>
2016-10-10 17:17:53 +0200

    Add class ProductA

---
+++ src/main/java/fr/paris10/poa/td3/main/ProductA.java
@@ -0,0 +1,11 @@
+package fr.paris10.poa.td3.main;
+
+/**
+ * Created by vabouque on 10/10/2016.
+ */
+public class ProductA {
+    public void doYourStuff() {
+        System.out.println("I'm a Product A, " +
+                "doing my stuff");
+    }
+}
