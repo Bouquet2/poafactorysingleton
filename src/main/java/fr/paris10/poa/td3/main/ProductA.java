4fca751b683e11b4ccc9a73705ed201245300be9
vabouque <valentin-bouquet@hotmail.fr>
2016-10-10 17:47:57 +0200

    Add method createProduct

--- src/main/java/fr/paris10/poa/td3/main/ProductA.java
+++ src/main/java/fr/paris10/poa/td3/main/ProductA.java
@@ -3,8 +3,14 @@ package fr.paris10.poa.td3.main;
 /**
  * Created by vabouque on 10/10/2016.
  */
-public class ProductA {
-    public void doYourStuff() {
+final class ProductA implements Product{
+
+    @Override
+    public void createProduct() {
+
+    }
+
+    public void doStuff() {
         System.out.println("I'm a Product A, " +
                 "doing my stuff");
     }
